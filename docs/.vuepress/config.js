c6500fd8fab57b8c2126baecd982c0d28c03c472
pengc <pengc@yunk.ai>
2020-09-30 08:34:50 +0800

    开生活篇-天道

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
       { text: '学习系列', link: '/study/' },
       { text: '思考系列', link: '/think/' },
       { text: '创业系列', link: '/work/' },
+      { text: '生活系列', link: '/life/' },
     ]
   }
 }
